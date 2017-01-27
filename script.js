6d669d2a6c8895aabe9468094a0384adbc07701e
Joseph Wunz <jodaman19@gmail.com>
2017-01-27 09:35:05 -0700

    Changed the labels for options to buttons.

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function loadComplete(evt) {
     console.log(triviaData);
     document.getElementById("question").innerHTML = triviaData.results[0].question;
     
-    document.getElementById("correct").innerHTML = triviaData.results[0].correct_answer;
-    document.getElementById("incorrect1").innerHTML = triviaData.results[0].incorrect_answers[0];
-    document.getElementById("incorrect2").innerHTML = triviaData.results[0].incorrect_answers[1];
-    document.getElementById("incorrect3").innerHTML = triviaData.results[0].incorrect_answers[2];
+    document.getElementById("correct").value = triviaData.results[0].correct_answer;
+    document.getElementById("incorrect1").value = triviaData.results[0].incorrect_answers[0];
+    document.getElementById("incorrect2").value = triviaData.results[0].incorrect_answers[1];
+    document.getElementById("incorrect3").value = triviaData.results[0].incorrect_answers[2];
     
 
 }
